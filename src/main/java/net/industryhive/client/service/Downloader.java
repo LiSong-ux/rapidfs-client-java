743c53f945651c9395b9be3cb34f99dd00744b30
LiSong-ux <546993317@qq.com>
2021-07-25 00:50:46 +0800

    修复文件下载操作文件流关闭问题

--- src/main/java/net/industryhive/client/service/Downloader.java
+++ src/main/java/net/industryhive/client/service/Downloader.java
@@ -11,44 +11,42 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import static net.industryhive.client.util.IOUtil.close;
+
 /**
  * @author LiSong-ux
  * @create 2021-07-24 17:20
  */
 public class Downloader {
     public static DataInputStream download(String filepath) {
-        Socket storageSocket = null;
-        DataOutputStream dos = null;
         DataInputStream dis = null;
+        Socket storageSocket = null;
+        RapidMessage contactMsg = Contactor.contact(filepath);
+        if (contactMsg.getStatus() != 200) {
+            System.out.println(contactMsg.getResult());
+            return null;
+        }
         try {
-            RapidMessage contactMsg = Contactor.contact(filepath);
-            if (contactMsg.getStatus() != 200) {
-                System.out.println(contactMsg.getResult());
-                return null;
-            }
-            storageSocket = new Socket(InetAddress.getByName(contactMsg.getResult()), Integer.parseInt(ConfigLoader.STORAGE_PORT));
-            dos = new DataOutputStream(storageSocket.getOutputStream());
+            InetAddress address = InetAddress.getByName(contactMsg.getResult());
+            int port = Integer.parseInt(ConfigLoader.STORAGE_PORT);
+            storageSocket = new Socket(address, port);
+
+            DataOutputStream dos = new DataOutputStream(storageSocket.getOutputStream());
             dis = new DataInputStream(storageSocket.getInputStream());
             dos.write(Contactor.getHeader((byte) 1, BaseProtocol.DOWNLOAD_COMMAND));
             dos.writeUTF(filepath);
             if (dis.read() != 210) {
                 String errorMsg = dis.readUTF();
                 System.out.println(errorMsg);
+                close(dis);
+                close(storageSocket);
                 return null;
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (dos != null) {
-                    dos.close();
-                }
-                if (storageSocket != null) {
-                    storageSocket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            close(dis);
+            close(storageSocket);
+            return null;
         }
         return dis;
     }
---
+++ src/main/java/net/industryhive/client/util/IOUtil.java
@@ -0,0 +1,47 @@
+package net.industryhive.client.util;
+
+import java.io.*;
+
+/**
+ * @author LiSong-ux
+ * @create 2021-07-24 15:48
+ */
+public class IOUtil {
+    public static <T extends Closeable> void close(T t) {
+        if (t != null) {
+            try {
+                t.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static boolean writeIn(InputStream source, File target) {
+        try (FileOutputStream fos = new FileOutputStream(target)) {
+            int length;
+            byte[] buffer = new byte[1024];
+            while ((length = source.read(buffer)) != -1) {
+                fos.write(buffer, 0, length);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean writeOut(File source, OutputStream target) {
+        try (FileInputStream fis = new FileInputStream(source)) {
+            int length;
+            byte[] buffer = new byte[1024];
+            while ((length = fis.read(buffer)) != -1) {
+                target.write(buffer, 0, length);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
